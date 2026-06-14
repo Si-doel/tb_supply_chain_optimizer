95c91260929ad06b8c735ad76af42338eef49538
Saefurrohman <saysaeful@gmail.com>
2026-06-14 13:37:15 +0700

    Revisi web.php

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('Pages.index');
+    return redirect()->route('dashboard');
 });
 
 Route::middleware(['auth', 'verified'])->group(function () {
@@ -18,24 +18,31 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/suppliers', function () {
-    return view('suppliers.index');
-})->name('suppliers.index');
+Route::middleware(['auth'])->group(function () {
 
-Route::get('/products', function () {
-    return view('products.index');
-})->name('products.index');
+    Route::get('/dashboard', [DashboardController::class, 'index'])
+        ->name('dashboard');
 
-Route::get('/transactions', function () {
-    return view('transactions.index');
-})->name('transactions.index');
+    Route::get('/suppliers', function () {
+        return view('suppliers.index');
+    })->name('suppliers.index');
 
-Route::get('/reorder/recommendations', function () {
-    return view('reorder.recommendations');
-})->name('reorder.recommendations');
+    Route::get('/products', function () {
+        return view('products.index');
+    })->name('products.index');
 
-Route::get('/reorder/drafts', function () {
-    return view('reorder.drafts');
-})->name('reorder.drafts');
+    Route::get('/transactions', function () {
+        return view('transactions.index');
+    })->name('transactions.index');
+
+    Route::get('/reorder/recommendations', function () {
+        return view('reorder.recommendations');
+    })->name('reorder.recommendations');
+
+    Route::get('/reorder/drafts', function () {
+        return view('reorder.drafts');
+    })->name('reorder.drafts');
+
+});
 
 require __DIR__.'/auth.php';
